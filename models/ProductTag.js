e7669451c04cd5f0d88e38b9f2473014d89344ca
mshaari <michael.shaari@gmail.com>
2023-02-13 17:11:57 -0500

    Changes to models, created .env file, import sequelize connection

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,28 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product', //case sensitive
+        key: 'id',
+        unique: false //DOES THIS NEED TO BE UNIQUE: TRUE???
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag', //case sensitive
+        key: 'id',
+        unique: false //DOES THIS NEED TO BE UNIQUE: TRUE???
+      }
+    }
   },
   {
     sequelize,
--- models/index.js
+++ models/index.js
@@ -5,12 +5,36 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_name',
+ }); 
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_name',
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  // Define an alias for when data is retrieved (tags associated with that product)
+  as: 'product_tags'
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  // Define an alias for when data is retrieved (products associated with that tag)
+  as: 'tag_products'
+})
 
 module.exports = {
   Product,
